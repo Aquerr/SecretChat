971026051a1488b5598c37e67cfc1a569fa55b9e
Aquerr <nerdianin@gmail.com>
2019-04-17 16:05:46 +0200

    Renamed gradle project to SecretChat

---
+++ src/main/java/io/github/aquerr/secretchat/SpringWebConfig.java
@@ -0,0 +1,16 @@
+package io.github.aquerr.secretchat;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class SpringWebConfig implements WebMvcConfigurer {
+
+//    @Override
+//    public void addResourceHandlers(ResourceHandlerRegistry registry)
+//    {
+//        registry.addResourceHandler("/static/css/**", "/static/js/**")
+//        .addResourceLocations("classpath:/css/", "classpath:/js/");
+//    }
+}
